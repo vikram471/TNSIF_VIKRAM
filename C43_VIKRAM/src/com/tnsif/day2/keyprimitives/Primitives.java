5ae1e104ec582a37e89d0e89895d20e8688bea75
vikram <darkdevils218@gmail.com>
2024-08-20 12:06:26 +0530

    comit

---
+++ C43_VIKRAM/src/com/tnsif/day2/keyprimitives/Primitives.java
@@ -0,0 +1,11 @@
+package com.tnsif.day2.keyprimitives;
+
+public class Primitives {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=4;
+    int b=3;
+    int c=a+b;
+		System.out.println(c);
+	}
+}
