64255be97119762c96db7fdda9633f2e9541c060
vikram <darkdevils218@gmail.com>
2024-08-20 12:01:37 +0530

    comit

---
+++ C43_VIKRAM/src/com/tnisf/day1/FirstProgram/Hello.java
@@ -0,0 +1,10 @@
+package com.tnisf.day1.FirstProgram;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("HelloWorld");
+	}
+
+}
